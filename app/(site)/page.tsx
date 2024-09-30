069b91d8755b0ddc1c984b938babb311ff1c8575
Samuele Stabile <77798622+SamueleStabile@users.noreply.github.com>
2024-09-30 13:29:17 +0200

    First Commit

---
+++ app/(site)/page.tsx
@@ -0,0 +1,10 @@
+
+import Header from "@/components/Header";
+
+export default function Home() {
+  return (
+    <div className="bg-background rounded-lg h-full w-full overflow-hidden overflox-y-auto">
+      <Header>Header</Header>
+    </div>
+  );
+}
---
+++ components/SidebarItem.tsx
@@ -0,0 +1,32 @@
+
+import Link from "next/link";
+// import { IconType } from "react-icons";
+import { twMerge } from "tailwind-merge";
+import { Icon } from 'iconsax-react';
+
+interface SidebarItemProps {
+    icon: Icon;
+    label: string;
+    active?: boolean;
+    href: string;
+}
+
+
+const SidebarItem: React.FC<SidebarItemProps> = ({
+    icon: Icon,
+    label,
+    active,
+    href
+}
+    
+) => {
+    
+    return ( 
+        <Link href={href} className={twMerge(`flex flex-row h-auto items-center w-full gap-x-4 text-md font-medium cursor-pointer  transition py-1`, active && "text-activemenu")}>
+        <Icon size={26}/>
+        <p className="truncate w-full">{label}</p>
+        </Link>
+     );
+}
+ 
+export default SidebarItem;
